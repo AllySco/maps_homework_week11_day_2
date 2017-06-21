6d40dd3ad3d234a50a0f4e206356799c84d5ff8e
Ally Gerrie <alasdairwgerrie@gmail.com>
2017-06-21 09:15:01 +0100

    adding files

---
+++ public/app.js
@@ -0,0 +1,26 @@
+var initialise = function() {
+  var coords = {lat: 57.13375, lng: -2.1100};
+  var zoom = 10;
+  var container = document.querySelector( "#main-map" );
+  var mainMap = new MapWrapper( container, coords, zoom);
+
+  var text = "Hello, I'm an info window"
+
+  mainMap.addMarker( coords );
+
+  mainMap.addClickEvent();
+
+  mainMap.addInfoWindow( coords, text );
+
+  var centreOnChicago = function() {
+  var chicago = { lat: 41.877, lng: -87.630 };
+      mainMap.setCenter( chicago )
+    }
+
+  var chicagoButton = document.querySelector( "#chicago-button" );
+
+  chicagoButton.addEventListener( 'click', centreOnChicago );
+
+}
+
+  window.addEventListener( "load", initialise );
---
+++ public/mapWrapper.js
@@ -0,0 +1,56 @@
+var MapWrapper = function(container, coords, zoom) {
+
+  this.googleMap = new google.maps.Map( container, {
+    center: coords,
+    zoom: zoom
+  });
+}
+
+MapWrapper.prototype = {
+
+  addMarker: function( coords ) {
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: this.googleMap
+    })
+  },
+
+  addClickEvent: function() {
+    this.googleMap.addListener( "click", function( event ) {
+      console.log( "map has been clicked" );
+      console.log( event.latLng.lat() );
+      console.log( event.latLng.lng() );
+      var coords = { lat: event.latLng.lat(), lng: event.latLng.lng() }
+      this.addMarker( coords )
+    }.bind( this ));
+  },
+
+  addInfoWindow: function(coords, text) {
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: this.googleMap
+    });
+    var infowindow = new google.maps.InfoWindow({
+      content: text,
+      maxWidth: 200
+    });
+    marker.addListener( 'click', function() {
+      infowindow.open( this.googleMap, marker );
+    });
+  }
+
+ // centreOnChicago: function( coords ){
+ //  this.googleMap.addListener( "click", function( event)
+ //    ) {
+ //      var coords = { coords }
+ //      this.addMarker( coords )
+ //    }.bind( this );
+ //  }
+ //  var marker = new google.maps.Marker({
+ //    position: coords,
+ //    map: this.googleMap
+ //  });
+ //  var setchicago = this.googleMap.setCenter( coords );
+ // }
+
+}
